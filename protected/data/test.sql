5e031c1f86b90fa83e08f52939d5486d2d75062e
piond <alfian@infest.or.id>
2015-11-01 07:30:09 +0700

    update db

--- protected/data/test.sql
+++ protected/data/test.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 30, 2015 at 01:01 AM
+-- Generation Time: Nov 01, 2015 at 01:29 AM
 -- Server version: 5.6.21
 -- PHP Version: 5.6.3
 
@@ -176,7 +176,7 @@ CREATE TABLE IF NOT EXISTS `tbl_users` (
 --
 
 INSERT INTO `tbl_users` (`id`, `username`, `password`, `email`, `activkey`, `createtime`, `lastvisit`, `superuser`, `status`) VALUES
-(1, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'webmaster@example.com', '9a24eff8c15a6a141ece27eb6947da0f', 1261146094, 1446081722, 1, 1),
+(1, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'webmaster@example.com', '9a24eff8c15a6a141ece27eb6947da0f', 1261146094, 1446279578, 1, 1),
 (2, 'demo', 'fe01ce2a7fbac8fafaed7c982a04e229', 'demo@example.com', '099f825543f7850cc038b90aaff39fac', 1261146096, 1446081699, 0, 1);
 
 -- --------------------------------------------------------
@@ -186,7 +186,7 @@ INSERT INTO `tbl_users` (`id`, `username`, `password`, `email`, `activkey`, `cre
 --
 
 CREATE TABLE IF NOT EXISTS `_article` (
-`id` int(11) NOT NULL,
+`article_id` int(11) NOT NULL,
   `title` text NOT NULL,
   `content` text,
   `createdAt` date NOT NULL,
@@ -195,20 +195,17 @@ CREATE TABLE IF NOT EXISTS `_article` (
   `author_id` int(11) NOT NULL,
   `views` int(11) DEFAULT '0',
   `category_id` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=79 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `_article`
 --
 
-INSERT INTO `_article` (`id`, `title`, `content`, `createdAt`, `modifiedAt`, `published`, `author_id`, `views`, `category_id`) VALUES
-(1, 'Lorem ipsum dolor sit amet', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', '2015-10-10', '2015-10-28', 0, 1, 1, 12),
-(10, 'Profile RSUD Kabupaten Temanggung', 'RSUD Kabupaten Temanggung sebagai Rumah Sakit Pemerintah di Kabupaten Temanggung memiliki peran dan tugas penting dalam menjamin kelangsungan dan mutu pelayanan kesehatan bagi seluruh masyarakat Temanggung. Selaku penyelenggara pelayanan kesehatan dalam mendukung tugas Pemerintah Daerah Kabupaten Temanggung, bertugas menyelenggarakan pelayanan kesehatan secara berdaya guna dan berhasil guna dengan mengutamakan upaya penyembuhan, pemulihan, peningkatan, pencegahan, pelayanan rujukan, dan menyelenggarakan pendidikan dan pelatihan, penelitian dan pengembangan serta pengabdian masyarakat.\r\n\r\n### RSUD Kabupaten Temanggung\r\n\r\n**Alamat :**\r\n\r\nJl. Dr. Sutomo No. 67 Temanggung\r\nTelp. 0293-491118, 491119\r\nFax 0293-493423\r\n\r\n**Status Kepemilikan** : Pemerintah Kabupaten Temanggung\r\n\r\n**Kelas Rumah Sakit** : B\r\n\r\n**Jumlah SDM** : 363 orang\r\n\r\nRSUD Kabupaten Temanggung menempati lahan seluas 25.885 m2 dengan kapasitas Tempat Tidur (TT) 203  TT, yang terdiri dari :\r\n\r\nVIP : 18 TT\r\n\r\nKelas utama : 18 TT\r\n\r\nKelas I : 36 TT\r\n\r\nKelas II : 34 TT\r\n\r\nKelas III : 88 TT\r\n\r\nIsolasi : 9 TT\r\n\r\nPerkembangan kinerja RSUD Kabupaten Temanggung dari tahun ke tahun menunjukkan perkembangan yang baik. Hal ini dapat dilihat dari salah satu indikator kinerja pelayanan yaitu Bed Occupation Rate (BOR) atau pemanfaatan tempat tidur di Rumah Sakit, yang cenderung terus meningkat dan telah mampu mencapai BOR ideal sesuai standar 60 - 80%. Dari aspek manajemen dan pengelolaan, RSUD Kabupaten Temanggung telah memiliki kemampuan yang cukup tinggi, terbukti dengan diperolehnya akreditasi penuh pada 16 jenis pelayanan pada tahun 2011.\r\nDengan terbitnya Undang-Undang RI Nomor 44 Tahun 2009 tentang Rumah Sakit, RSUD Kabupaten Temanggung mengusulkan untuk dapat menerapkan Pola Pengelolaan Keuangan Badan Layanan Umum Daerah (PPK-BLUD), dan dengan Keputusan Bupati Temanggung Nomor 440/448 Tahun 2011, RSUD Kabupaten Temanggung menerapkan Pola Pengelolaan Keuangan Badan Layanan Umum Daerah (PPK-BLUD) per 1 Januari 2012. Penerapan BLUD akan membuat RSUD Kabupaten Temanggung lebih responsif dan agresif dalam menghadapi tuntutan masyarakat dan eskalasi perubahan yang begitu cepat dengan cara melaksanakan prinsip-prinsip ekonomi yang efektif dan efisien, namun tidak meninggalkan jati dirinya dalam mengemban misi sosial dalam memenuhi kebutuhan masyarakat.\r\n', '2015-10-27', '2015-10-28', 1, 1, 0, 1),
+INSERT INTO `_article` (`article_id`, `title`, `content`, `createdAt`, `modifiedAt`, `published`, `author_id`, `views`, `category_id`) VALUES
+(1, 'Lorem ipsum dolor sit amet', '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>\r\n', '2015-10-10', '2015-10-31', 0, 1, 1, 5),
+(10, 'Profile RSUD Kabupaten Temanggung', '<p>RSUD Kabupaten Temanggung sebagai Rumah Sakit Pemerintah di Kabupaten Temanggung memiliki peran dan tugas penting dalam menjamin kelangsungan dan mutu pelayanan kesehatan bagi seluruh masyarakat Temanggung. Selaku penyelenggara pelayanan kesehatan dalam mendukung tugas Pemerintah Daerah Kabupaten Temanggung, bertugas menyelenggarakan pelayanan kesehatan secara berdaya guna dan berhasil guna dengan mengutamakan upaya penyembuhan, pemulihan, peningkatan, pencegahan, pelayanan rujukan, dan menyelenggarakan pendidikan dan pelatihan, penelitian dan pengembangan serta pengabdian masyarakat.</p>\r\n\r\n<p><strong>RSUD Kabupaten Temanggung</strong></p>\r\n\r\n<p><strong>Alamat :</strong> Jl. Dr. Sutomo No. 67 Temanggung Telp. 0293-491118, 491119 Fax 0293-493423</p>\r\n\r\n<p><strong>Status Kepemilikan :</strong> Pemerintah Kabupaten Temanggung</p>\r\n\r\n<p><strong>Kelas Rumah Sakit :</strong> B</p>\r\n\r\n<p><strong>Jumlah SDM :</strong> 363 orang</p>\r\n\r\n<p>RSUD Kabupaten Temanggung menempati lahan seluas 25.885 m2 dengan kapasitas Tempat Tidur (TT) 203 TT, yang terdiri dari : VIP : 18 TT Kelas utama : 18 TT Kelas I : 36 TT Kelas II : 34 TT Kelas III : 88 TT Isolasi : 9 TT</p>\r\n\r\n<p>Perkembangan kinerja RSUD Kabupaten Temanggung dari tahun ke tahun menunjukkan perkembangan yang baik. Hal ini dapat dilihat dari salah satu indikator kinerja pelayanan yaitu Bed Occupation Rate (BOR) atau pemanfaatan tempat tidur di Rumah Sakit, yang cenderung terus meningkat dan telah mampu mencapai BOR ideal sesuai standar 60 - 80%. Dari aspek manajemen dan pengelolaan, RSUD Kabupaten Temanggung telah memiliki kemampuan yang cukup tinggi, terbukti dengan diperolehnya akreditasi penuh pada 16 jenis pelayanan pada tahun 2011. Dengan terbitnya Undang-Undang RI Nomor 44 Tahun 2009 tentang Rumah Sakit, RSUD Kabupaten Temanggung mengusulkan untuk dapat menerapkan Pola Pengelolaan Keuangan Badan Layanan Umum Daerah (PPK-BLUD), dan dengan Keputusan Bupati Temanggung Nomor 440/448 Tahun 2011, RSUD Kabupaten Temanggung menerapkan Pola Pengelolaan Keuangan Badan Layanan Umum Daerah (PPK-BLUD) per 1 Januari 2012. Penerapan BLUD akan membuat RSUD Kabupaten Temanggung lebih responsif dan agresif dalam menghadapi tuntutan masyarakat dan eskalasi perubahan yang begitu cepat dengan cara melaksanakan prinsip-prinsip ekonomi yang efektif dan efisien, namun tidak meninggalkan jati dirinya dalam mengemban misi sosial dalam memenuhi kebutuhan masyarakat.</p>\r\n', '2015-10-27', '2015-10-30', 1, 1, 0, 1),
 (15, 'Welcome to RSUD Temanggung', '<p>\r\n		Congratulations! You have successfully created your Yii application.\r\n	</p>\r\n\r\n	<p>\r\n		You may change the content of this page by modifying the following two files:\r\n	</p>\r\n	\r\n	<ul>\r\n		<li>View file: <code><?php echo __FILE__; ?></code></li>\r\n		<li>Layout file: <code><?php echo $this->getLayoutFile(''main''); ?></code></li>\r\n	</ul>\r\n\r\n	<p>\r\n		For more details on how to further develop this application, please read\r\n		the <a href="http://www.yiiframework.com/doc/">documentation</a>.\r\n		Feel free to ask in the <a href="http://www.yiiframework.com/forum/">forum</a>,\r\n		should you have any questions.\r\n	</p>', '2015-10-28', '2015-10-28', 0, 1, 0, 1),
-(16, 'a', 'a', '2015-10-28', '2015-10-28', 0, 1, 0, 1),
-(17, 'a', 'a', '2015-10-28', '2015-10-28', 0, 1, 0, 1),
-(18, 'ASD', '', '2015-10-29', '2015-10-29', 0, 1, 0, 5),
-(19, 'asd', '', '2015-10-29', '2015-10-29', 0, 1, 0, 4);
+(20, 'Judul-judulan', '<h2>Introduction</h2>\r\n\r\n<p>Grid systems are used for creating page layouts through a series of rows and columns that house your content. Here&#39;s how the Bootstrap grid system works:</p>\r\n\r\n<ul>\r\n	<li>Rows must be placed within a <code>.container</code> (fixed-width) or <code>.container-fluid</code> (full-width) for proper alignment and padding.</li>\r\n	<li>Use rows to create horizontal groups of columns.</li>\r\n	<li>Content should be placed within columns, and only columns may be immediate children of rows.</li>\r\n	<li>Predefined grid classes like <code>.row</code> and <code>.col-xs-4</code> are available for quickly making grid layouts. Less mixins can also be used for more semantic layouts.</li>\r\n	<li>Columns create gutters (gaps between column content) via <code>padding</code>. That padding is offset in rows for the first and last column via negative margin on <code>.row</code>s.</li>\r\n	<li>The negative margin is why the examples below are outdented. It&#39;s so that content within grid columns is lined up with non-grid content.</li>\r\n	<li>Grid columns are created by specifying the number of twelve available columns you wish to span. For example, three equal columns would use three <code>.col-xs-4</code>.</li>\r\n	<li>If more than 12 columns are placed within a single row, each group of extra columns will, as one unit, wrap onto a new line.</li>\r\n	<li>Grid classes apply to devices with screen widths greater than or equal to the breakpoint sizes, and override grid classes targeted at smaller devices. Therefore, e.g. applying any <code>.col-md-*</code> class to an element will not only affect its styling on medium devices but also on large devices if a <code>.col-lg-*</code> class is not present.</li>\r\n</ul>\r\n\r\n<p>Look to the examples for applying these principles to your code.</p>\r\n', '2015-10-31', '2015-10-31', 0, 1, 0, 5);
 
 -- --------------------------------------------------------
 
@@ -219,8 +216,25 @@ INSERT INTO `_article` (`id`, `title`, `content`, `createdAt`, `modifiedAt`, `pu
 CREATE TABLE IF NOT EXISTS `_articletags` (
 `id` int(11) NOT NULL,
   `article_id` int(11) NOT NULL,
-  `tag_id1` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `tag_id` int(11) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `_articletags`
+--
+
+INSERT INTO `_articletags` (`id`, `article_id`, `tag_id`) VALUES
+(1, 76, 2),
+(2, 76, 1),
+(3, 76, 16),
+(4, 77, 17),
+(5, 77, 11),
+(6, 77, 10),
+(7, 1, 2),
+(8, 1, 1),
+(9, 1, 18),
+(10, 1, 2),
+(11, 20, 18);
 
 -- --------------------------------------------------------
 
@@ -229,7 +243,7 @@ CREATE TABLE IF NOT EXISTS `_articletags` (
 --
 
 CREATE TABLE IF NOT EXISTS `_categories` (
-`id` int(11) NOT NULL,
+`category_id` int(11) NOT NULL,
   `category` varchar(32) NOT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
 
@@ -237,7 +251,7 @@ CREATE TABLE IF NOT EXISTS `_categories` (
 -- Dumping data for table `_categories`
 --
 
-INSERT INTO `_categories` (`id`, `category`) VALUES
+INSERT INTO `_categories` (`category_id`, `category`) VALUES
 (1, 'Instalasi Gawat Darurat'),
 (2, 'Instalasi Rawat Jalan'),
 (3, 'Instalasi Rawat Inap'),
@@ -258,7 +272,7 @@ INSERT INTO `_categories` (`id`, `category`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `_settings` (
-`id` int(11) NOT NULL,
+`setting_id` int(11) NOT NULL,
   `name` varchar(32) NOT NULL,
   `description` text NOT NULL,
   `content` text NOT NULL,
@@ -269,7 +283,7 @@ CREATE TABLE IF NOT EXISTS `_settings` (
 -- Dumping data for table `_settings`
 --
 
-INSERT INTO `_settings` (`id`, `name`, `description`, `content`, `type`) VALUES
+INSERT INTO `_settings` (`setting_id`, `name`, `description`, `content`, `type`) VALUES
 (1, 'Profile RSUD Kabupaten Temanggun', 'RSUD Kabupaten Temanggung sebagai Rumah Sakit Pemerintah di Kabupaten Temanggung memiliki peran dan tugas penting dalam menjamin kelangsungan dan mutu pelayanan kesehatan bagi seluruh masyarakat Temanggung. Selaku penyelenggara pelayanan kesehatan dalam mendukung tugas Pemerintah Daerah Kabupaten Temanggung, bertugas menyelenggarakan pelayanan kesehatan secara berdaya guna dan berhasil guna dengan mengutamakan upaya penyembuhan, pemulihan, peningkatan, pencegahan, pelayanan rujukan, dan menyelenggarakan pendidikan dan pelatihan, penelitian dan pengembangan serta pengabdian masyarakat. ### RSUD Kabupaten Temanggung **Alamat :** Jl. Dr. Sutomo No. 67 Temanggung Telp. 0293-491118, 491119 Fax 0293-493423 **Status Kepemilikan** : Pemerintah Kabupaten Temanggung **Kelas Rumah Sakit** : B **Jumlah SDM** : 363 orang RSUD Kabupaten Temanggung menempati lahan seluas 25.885 m2 dengan kapasitas Tempat Tidur (TT) 203 TT, yang terdiri dari : VIP : 18 TT Kelas utama : 18 TT Kelas I : 36 TT Kelas II : 34 TT Kelas III : 88 TT Isolasi : 9 TT Perkembangan kinerja RSUD Kabupaten Temanggung dari tahun ke tahun menunjukkan perkembangan yang baik. Hal ini dapat dilihat dari salah satu indikator kinerja pelayanan yaitu Bed Occupation Rate (BOR) atau pemanfaatan tempat tidur di Rumah Sakit, yang cenderung terus meningkat dan telah mampu mencapai BOR ideal sesuai standar 60 - 80%. Dari aspek manajemen dan pengelolaan, RSUD Kabupaten Temanggung telah memiliki kemampuan yang cukup tinggi, terbukti dengan diperolehnya akreditasi penuh pada 16 jenis pelayanan pada tahun 2011. Dengan terbitnya Undang-Undang RI Nomor 44 Tahun 2009 tentang Rumah Sakit, RSUD Kabupaten Temanggung mengusulkan untuk dapat menerapkan Pola Pengelolaan Keuangan Badan Layanan Umum Daerah (PPK-BLUD), dan dengan Keputusan Bupati Temanggung Nomor 440/448 Tahun 2011, RSUD Kabupaten Temanggung menerapkan Pola Pengelolaan Keuangan Badan Layanan Umum Daerah (PPK-BLUD) per 1 Januari 2012. Penerapan BLUD akan membuat RSUD Kabupaten Temanggung lebih responsif dan agresif dalam menghadapi tuntutan masyarakat dan eskalasi perubahan yang begitu cepat dengan cara melaksanakan prinsip-prinsip ekonomi yang efektif dan efisien, namun tidak meninggalkan jati dirinya dalam mengemban misi sosial dalam memenuhi kebutuhan masyarakat.', '', 'profiles'),
 (2, 'First thumbnail', 'First caption', 'cover.jpg', 'banner'),
 (3, 'Instalasi Gawat Darurat', '', '', 'instalasi');
@@ -281,16 +295,32 @@ INSERT INTO `_settings` (`id`, `name`, `description`, `content`, `type`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `_tags` (
-`id` int(11) NOT NULL,
+`tag_id` int(11) NOT NULL,
   `tag` varchar(20) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `_tags`
 --
 
-INSERT INTO `_tags` (`id`, `tag`) VALUES
-(1, 'kesehatan');
+INSERT INTO `_tags` (`tag_id`, `tag`) VALUES
+(1, 'kesehatan'),
+(2, 'bedah'),
+(4, 'sss'),
+(5, 'aaaa'),
+(6, 'ffff'),
+(7, 'gggg'),
+(8, 'aduh'),
+(9, 'asem'),
+(10, 'yes'),
+(11, 'no'),
+(12, 'a'),
+(13, 'ssss'),
+(14, 'xxxx'),
+(15, 'alfian'),
+(16, 'rizal'),
+(17, 'duh'),
+(18, '');
 
 --
 -- Indexes for dumped tables
@@ -342,7 +372,7 @@ ALTER TABLE `tbl_users`
 -- Indexes for table `_article`
 --
 ALTER TABLE `_article`
- ADD PRIMARY KEY (`id`);
+ ADD PRIMARY KEY (`article_id`);
 
 --
 -- Indexes for table `_articletags`
@@ -354,19 +384,19 @@ ALTER TABLE `_articletags`
 -- Indexes for table `_categories`
 --
 ALTER TABLE `_categories`
- ADD PRIMARY KEY (`id`);
+ ADD PRIMARY KEY (`category_id`);
 
 --
 -- Indexes for table `_settings`
 --
 ALTER TABLE `_settings`
- ADD PRIMARY KEY (`id`);
+ ADD PRIMARY KEY (`setting_id`);
 
 --
 -- Indexes for table `_tags`
 --
 ALTER TABLE `_tags`
- ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `tag` (`tag`);
+ ADD PRIMARY KEY (`tag_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -386,27 +416,27 @@ MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 -- AUTO_INCREMENT for table `_article`
 --
 ALTER TABLE `_article`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=20;
+MODIFY `article_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=79;
 --
 -- AUTO_INCREMENT for table `_articletags`
 --
 ALTER TABLE `_articletags`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=12;
 --
 -- AUTO_INCREMENT for table `_categories`
 --
 ALTER TABLE `_categories`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=13;
+MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=13;
 --
 -- AUTO_INCREMENT for table `_settings`
 --
 ALTER TABLE `_settings`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+MODIFY `setting_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `_tags`
 --
 ALTER TABLE `_tags`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+MODIFY `tag_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=19;
 --
 -- Constraints for dumped tables
 --
